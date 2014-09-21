607c968bb7e4f3dff8f2aa501a5261ecf385ccec
shikevin <im@shikev.in>
2014-09-21 00:56:05 -0400

    there are errors, code not working

--- public/js/app.js
+++ public/js/app.js
@@ -1,58 +1,82 @@
-jQuery(function($) {
-  'use strict';
+jQuery(function ($) {
+    'use strict';
 
-  var timeout = 3000;
+    var $endCall = $('#end-call');
+    var $startCall = $('#start-call');
+    var timeout = 3000;
+    var callers = [];
+    var connect = null;
 
-  function setup() {
-    $.post('http://apollo.mantasmatelis.com/client/new', null, function(data, status) {
-      console.log(data);
-      if (status === "success") {
-        twilioSetup(data);
-      }
-    });
-  }
+    function generateCallers() {
+        for (var i = 0; i < 10; i++) {
+            callers.push('+' + Math.floor(10000000000 + Math.random() * 90000000000));
+        }
+    };
 
-  function pingServer(id) {
-      $.post('http://apollo.mantasmatelis.com/client/' + id + '/ping', null, function(data, status){
-          console.log(status);
-      });
-  }
+    function setup() {
+        $.post('http://apollo.mantasmatelis.com/client/new', null, function (data, status) {
+            console.log(data);
+            if (status === "success") {
+                twilioSetup(data);
+            }
+        });
+    }
 
-  function twilioSetup(data) {
-    timerPost(data.id);
-    console.log(data.id);
-    console.log(data.token);
-    Twilio.Device.setup(data.token);
-  }
+    function pingServer(id) {
+        $.post('http://apollo.mantasmatelis.com/client/' + id + '/ping', null, function (data, status) {
+            console.log(status);
+        });
+    }
+
+    function twilioSetup(data) {
+        timerPost(data.id);
+        console.log(data.id);
+        console.log(data.token);
+        Twilio.Device.setup(data.token);
+    }
 
     function timerPost(id) {
         pingServer(id);
-        setTimeout(function() {
+        setTimeout(function () {
             timerPost(id)
         }, timeout);
     }
 
-    Twilio.Device.ready(function(device){
-      var $outNumber = $('#outbound-call');
-      function outCall(number){
-        $outNumber.val('');
-      }
+    $startCall.click(function() {
+        if (connect == null) return;
+        connect.accept();
+        callers.push(connect.parameters.From);
+        console.log(callers);
+        console.log('connected');
+        $endCall.click(function () {
+            device.disconnectAll();
+        });
+    });
 
-      device.incoming(function(connection) {
-        if (window.confirm("Incoming Call")) {
-            
-            connection.accept();
-            console.log('connected');
+    Twilio.Device.ready(function (device) {
+
+        var $outNumber = $('#outbound-call');
+        function outCall(number) {
+            $outNumber.val('');
         }
-        connection.reject();
-      });
 
-      $outNumber.submit(function(oEvent){
-          oEvent.preventDefault();
-          outCall($outNumber.val());
-      })
-  })
+        device.incoming(function (connection) {
 
-  setup();
+
+            connect = connection;
+
+            $endCall.click(function () {
+                connection.reject();
+            })
+        });
+
+        $outNumber.submit(function (oEvent) {
+            oEvent.preventDefault();
+            outCall($outNumber.val());
+        })
+    })
+
+    generateCallers();
+    setup();
 
 });
