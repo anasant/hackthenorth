4e13cbb9cf226be4a4bf8302774f8251030a8427
Anastasia Santasheva <anastasia-sant@hotmail.com>
2014-09-21 01:32:51 -0400

    fixes to file structure

--- app.js
+++
@@ -1,7 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-
-var app = connect();
-
-app.use(serveStatic('public', {'index': ['index.html']}));
-app.listen(3000);
