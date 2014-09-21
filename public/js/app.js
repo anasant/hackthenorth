368c4f54039393ca2eded6283fab38b61acdd882
shikevin <im@shikev.in>
2014-09-20 23:25:03 -0400

    pings to keep alive is working

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,8 @@
 jQuery(function($) {
   'use strict';
 
+  var timeout = 3000;
+
   function setup() {
     $.post('http://apollo.mantasmatelis.com/client/new', null, function(data, status) {
       console.log(data);
@@ -10,19 +12,35 @@ jQuery(function($) {
     });
   }
 
-  function twilioSetup(token) {
-    Twilio.Device.setup(token);
+  function pingServer(id) {
+      $.post('http://apollo.mantasmatelis.com/client/' + id + '/ping', null, function(data, status){
+          console.log(status);
+      });
   }
 
-  Twilio.Device.ready(function(device){
-      var $outNumber = $('#outbound-call');
+  function twilioSetup(data) {
+    timerPost(data.id);
+    console.log(data.id);
+    console.log(data.token);
+    Twilio.Device.setup(data.token);
+  }
 
+    function timerPost(id) {
+        pingServer(id);
+        setTimeout(function() {
+            timerPost(id)
+        }, timeout);
+    }
+
+    Twilio.Device.ready(function(device){
+      var $outNumber = $('#outbound-call');
       function outCall(number){
         $outNumber.val('');
       }
 
       device.incoming(function(connection) {
         if (window.confirm("Incoming Call")) {
+            
             connection.accept();
             console.log('connected');
         }
